5f9b37fbc07baddf9cd0146fc8f0468005d9ab03
Diego Guerrero <guerrero-romero-diego@hotmail.com>
2023-11-29 20:09:16 -0600

    entrega 2 de curso js con estructura html y css y js corregido

--- js/index.js
+++ js/index.js
@@ -1,69 +1,57 @@
+class Tarea{
+    constructor(nombre, completada = false){
+        this.nombre = nombre;
+        this.completada = completada;
+    }
+}
+
 const tareas = [];
 
+const btnAgregar = document.getElementById('btnAgregar');
+btnAgregar.addEventListener('click',agregarTarea);
+
 function agregarTarea() {
-    const tarea = prompt('Ingrese una nueva tarea:');
-    tareas.push({ nombre: tarea, completada: false });
-    console.log(`Tarea "${tarea}" agregada.`);
+    const tareaNombre = prompt('Ingrese una nueva tarea:');
+    const nuevaTarea = new Tarea(tareaNombre);
+    tareas.push(nuevaTarea);
+
+    actualizarTareas();
 }
 
-function listarTareas() {
-    console.log('Lista de tareas:');
-    for (let i = 0; i < tareas.length; i++) {
-        const estado = tareas[i].completada ? 'Completada' : 'Pendiente';
-        console.log(`${i + 1}. ${tareas[i].nombre} (${estado})`);
-    }
+function actualizarTareas() {
+    const listaTareas = document.getElementById('tareas-lista');
+    listaTareas.innerHTML = '';
+
+    tareas.forEach((tarea, i) => {
+        const estado = tarea.completada ? 'Completada' : 'Pendiente';
+        const listItem = document.createElement('li');
+        listItem.textContent = `${i + 1}. ${tarea.nombre} (${estado})`;
+
+        listItem.onclick = () => cambiarEstadoTarea(i);
+        listaTareas.appendChild(listItem);
+    });
 }
 
-function cambiarEstadoTarea() {
-    const indice = parseInt(prompt('Ingrese el número de la tarea que desea completar:'));
-    if (isNaN(indice) || indice < 1 || indice > tareas.length) {
-        console.log('Número de tarea no válido. Inténtelo de nuevo.');
-    } else {
-        tareas[indice - 1].completada = true;
-        console.log(`Tarea "${tareas[indice - 1].nombre}" marcada como completada.`);
-    }
+function cambiarEstadoTarea(i) {
+    tareas[i].completada = !tareas[i].completada;
+    actualizarTareas();
 }
 
-function eliminarTarea() {
-    const indice = parseInt(prompt('Ingrese el número de la tarea que desea eliminar:'));
-    if (isNaN(indice) || indice < 1 || indice > tareas.length) {
-        console.log('Número de tarea no válido. Inténtelo de nuevo.');
-    } else {
-        const tareaEliminada = tareas.splice(indice - 1, 1);
-        console.log(`Tarea "${tareaEliminada[0].nombre}" eliminada.`);
-    }
+const btnEliminar = document.getElementById('btnEliminar');
+btnEliminar.addEventListener('click',eliminarTarea);
+
+function eliminarTarea(i) {
+    tareas.splice(i, 1);
+    actualizarTareas();
 }
+
+const btnSalir = document.getElementById('btnSalir');
+btnSalir.addEventListener('click',salirTareas);
 
 function salirTareas(){
-    salir = prompt("¿Seguro que desea salir? (SI / NO)").toLowerCase();
-    if (salir === "si"){
+    const salir = confirm("¿Seguro que desea salir?");
+    if (salir){
         alert("Ha salido del programa");
         process.exit(0);
-    } else {
-        console.log("Elija de nuevo.");
     }
 }
-
-while (true) {
-    const opcion = prompt('Elija una opción:\n1. Agregar tarea\n2. Listar tareas\n3. Completar tarea\n4. Eliminar tarea\n5. Salir');
-
-    switch (opcion) {
-        case '1':
-            agregarTarea();
-            break;
-        case '2':
-            listarTareas();
-            break;
-        case '3':
-            cambiarEstadoTarea();
-            break;
-        case '4':
-            eliminarTarea();
-            break;
-        case '5':
-            salirTareas();
-        default:
-            console.log('Opción no válida. Por favor, seleccione una opción válida.');
-    }
-}
-
